6c5f3b57f0e568abb6c3548124daea49e3ad0fe3
Rich Polanco <rich.polanco@automattic.com>
2023-06-24 22:30:35 -0600

    Sheet added

--- Toolbar Project/ContentView.swift
+++ Toolbar Project/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isPresenting: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Text("Initial View")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: {
+                            isPresenting = true
+                        }, label: {
+                            Image(systemName: "gear")
+                        })
+                    }
+                }
+                .sheet(isPresented: $isPresenting) {
+                    NavigationView {
+                        Text("Settings Page")
+                            .toolbar {
+                                ToolbarItem {
+                                    Button("Save") {}
+                                }
+                            }
+                    }
+                }
         }
-        .padding()
     }
 }
 
