3431f362dbd87bd06cf53b3f6419a13af4bd5d41
Rich Polanco <rich.polanco@automattic.com>
2023-06-24 21:24:34 -0600

    Initial Commit

---
+++ Toolbar Project/Toolbar_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toolbar_ProjectApp.swift
+//  Toolbar Project
+//
+//  Created by Rich Polanco on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Toolbar_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
